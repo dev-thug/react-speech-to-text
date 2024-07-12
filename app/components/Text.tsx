64b5331a1be2203957e727c29ad2524e4468c6b0
dev-thug <de0978@gmail.com>
2024-07-12 18:40:14 +0900

    feat: init

--- app/components/Text.tsx
+++ app/components/Text.tsx
@@ -19,20 +19,37 @@ const Text: FC<TextProps> = ({}) => {
     return <span>Browser does not support speech recognition.</span>;
   }
 
+  const startListening = () => {
+    SpeechRecognition.startListening({ continuous: true });
+  };
+
   return (
     <div>
       <h1 className="lg:text-5xl font-bold underline decoration-wavy text-2xl">
-        Speech to text
+        Bird
       </h1>
-      <p className=" mt-6 pb-32 mb-4 rounded-md bg-base-100 lg:w-96 lg:h-48 w-64 h-64">
-        <span className="ml-2 font-bold text-xl bg-base-100">generated text:</span>
+      <p className="mt-6 pb-32 mb-4 rounded-md bg-base-100 lg:w-96 lg:h-48 w-64 h-64">
+        <span className="ml-2 font-bold text-xl bg-base-100">
+          generated text:
+        </span>
         {transcript}
       </p>
-        <p className="mb-2 text-xl font-bold">Microphone: {listening ? 'Listing to your voice..' : 'off'}</p>
+      <p className="mb-2 text-xl font-bold">
+        Microphone: {listening ? "Listening to your voice..." : "off"}
+      </p>
       <div className="flex gap-3">
-        <button className="btn btn-primary btn-sm" onClick={SpeechRecognition.startListening}>Start</button>
-        <button className="btn btn-secondary btn-sm" onClick={SpeechRecognition.stopListening}>Stop</button>
-        <button className="btn btn-accent btn-sm" onClick={resetTranscript}>Reset</button>
+        <button className="btn btn-primary btn-sm" onClick={startListening}>
+          Start
+        </button>
+        <button
+          className="btn btn-secondary btn-sm"
+          onClick={SpeechRecognition.stopListening}
+        >
+          Stop
+        </button>
+        <button className="btn btn-accent btn-sm" onClick={resetTranscript}>
+          Reset
+        </button>
       </div>
     </div>
   );
